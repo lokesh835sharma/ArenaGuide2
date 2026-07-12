67ddae1bfe854c5681b31e83f145674f1ff9fc1d
Lokesh Sharma <lokesh835sharma@github.com>
2026-07-12 10:13:05 +0530

    feat: Add Netlify Serverless Functions support

---
+++ netlify/functions/api.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+# Add project root to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+from src.main import app
+from mangum import Mangum
+
+handler = Mangum(app)
